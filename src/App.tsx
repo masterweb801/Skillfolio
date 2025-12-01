b470608775dd376361adae574e49385db0e8069c
MD Mobashshirul Karim <masterweb801@gmail.com>
2025-12-02 01:40:21 +0600

    Rebuilding Start with React, Tailwind and TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="min-h-screen w-full overflow-x-hidden">
+      <div className="text-xl">Hello, Tailwind!</div>
+    </main>
+  )
+}
+
+export default App
